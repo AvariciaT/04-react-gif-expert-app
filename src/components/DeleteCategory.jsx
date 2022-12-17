34f95c6ba15d0a2572aaf99ce9374f1e8c754cf9
AvariciaT <83774625+AvariciaT@users.noreply.github.com>
2022-12-17 03:00:34 -0500

    Boton para eliminar, listo.

--- src/components/DeleteCategory.jsx
+++ src/components/DeleteCategory.jsx
@@ -5,7 +5,7 @@ export const DeleteCategory = ( props ) => {
 
   const [ stateItem, setStateItem ] = useState( props.state );
 
-  const singleDelete = () => {
+  const singleDelete = (event) => {
         // Cambiamos el estado a true, para eliminar
         setStateItem(true); 
         // Verificar si está listo para eliminar
@@ -17,12 +17,27 @@ export const DeleteCategory = ( props ) => {
             console.log(newListCategories);
             props.setCategories(newListCategories); 
 
+        }else{
+          
+          // console.log( event.target.parentElement.parentElement.children[0].innerHTML);
+          [...document.querySelectorAll('.headSearch__nameSearch')].forEach((element, index)=>{
+
+            if(document.querySelectorAll('.headSearch__nameSearch')[index].innerHTML == `${props.category}`){
+              // console.log(element); dbz  
+              // console.log('Listo para borrar'); 
+              element.nextElementSibling.children[0].style.background='orange';
+              element.nextElementSibling.children[0].children[0].style.background='orange';
+            }
+
+          });
+
         }
+
     }
 
   return (
     <>
-        <div className="buttonsSearch">{stateItem+']'}
+        <div className="buttonsSearch">
             <div className="buttonsSearch__item" onClick={ singleDelete }>
                 <div className="buttonsSearch__delete close icon"></div>
             </div>
