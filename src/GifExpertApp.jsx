e85706da31156a19fd1b8fbdec6780ff6621cc51
AvariciaT <83774625+AvariciaT@users.noreply.github.com>
2022-12-18 22:22:18 -0500

    Modifiqué el titulo del proyecto MAIN

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -47,7 +47,7 @@ export const GifExpertApp = () => {
 
   return (
     <>
-      <h1 className="inline-flex w-full justify-center rounded-md border border-transparent bg-red-700 px-4 py-2 text-base font-medium text-white shadow-sm focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 sm:text-2xl">GifExpertApp</h1>
+      <h1 className="inline-flex w-full justify-center rounded-md border border-transparent bg-red-700 px-4 py-2 text-base font-medium text-white shadow-sm focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 sm:text-2xl">GifExpertApp-Main</h1>
       <AddCategory 
       onNewCategory={event=> onAddCategory(event)}
       />
